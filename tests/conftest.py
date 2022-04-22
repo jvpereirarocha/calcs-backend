568e4bec4e150211bf0beb78b4f7752e39b4600a
Joao Victor <jvpereirarocha@gmail.com>
2022-04-21 21:35:44 -0300

    add new mocks into imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,4 @@ from tests.mocks.mocks_domain.mock_expenses import mock_expense, mock_expense_ge
 from tests.mocks.mocks_domain.mock_accounts import mock_account, mock_account_generate
 from tests.mocks.mocks_domain.mock_revenues import mock_revenue, mock_revenue_generate
 from tests.mocks.mocks_domain.mocks_balance import mock_balance, mock_balance_generate
+from tests.mocks.mocks_domain.mock_persons import mock_person, mock_person_generate
