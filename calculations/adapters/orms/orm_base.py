26f3ef05f17a870ea00ac62f58341d67c74be628
Joao Victor <jvpereirarocha@gmail.com>
2022-04-23 01:20:03 -0300

    create new orm from expense model

---
+++ calculations/adapters/orms/orm_base.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from email.policy import default
+from typing import Optional, Type
+import uuid
+from sqlalchemy import DateTime
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy import MetaData
+from sqlalchemy import func, util
+from sqlalchemy import types
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class CustomUUID(types.UserDefinedType):
+    cache_ok = True
+    __visit_name__ = 'UUID'
+
+    def __init__(self, class_type: Optional[Type[uuid.UUID]]) -> None:
+        if class_type:
+            assert issubclass(class_type, uuid.UUID), ValueError(
+                "Should be an UUID subclass"
+            )
+            self.class_type = class_type
+        else:
+            self.class_type = uuid.UUID
+        
+        self.as_uuid = True
+
+    def bind_processor(self, dialect):
+        def process(value):
+            if value is not None:
+                value = util.text_type(value)
+            return value
+        
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            if value is not None:
+                value = self.class_type(str(value))
+            return value
+
+        return process
+
+
+class CustomColumn(Column):
+    def __init__(self, *args, **kwargs) -> None:
+        if "nullable" not in kwargs:
+            kwargs["nullable"] = False
+        
+        super().__init__(*args, **kwargs)
+    
+    @staticmethod
+    def UUID_as_primary_key(field_name: str, class_type=None):
+        return Column(
+            field_name,
+            CustomUUID(class_type=class_type),
+            primary_key=True,
+            default=uuid.uuid4,
+            unique=True,
+            nullable=False
+        )
+
+    def UUIDField(field_name: str, class_type=None, nullable=False, **kwargs):
+        return Column(
+            field_name,
+            CustomUUID(class_type=class_type),
+            default=uuid.uuid4,
+            **kwargs,
+            nullable=False
+        )
+
+class CustomTable(Table):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def _init(cls, *args, **kwargs):
+        super()._init(
+            *args,
+            CustomColumn('created_when', DateTime, default=datetime.utcnow, nullable=False),
+            CustomColumn('last_modified', DateTime, server_default=func.now(), server_onupdate=func.now(), onupdate=datetime.utcnow, nullable=False),
+            **kwargs
+        )
--- calculations/adapters/orms/orm_expenses.py
+++ calculations/adapters/orms/orm_expenses.py
@@ -1,1 +1,22 @@
+from datetime import datetime
+from sqlalchemy import Boolean
+from sqlalchemy import DateTime
+from sqlalchemy import String, Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import MetaData
+from calculations.adapters.orms.orm_base import CustomColumn, CustomTable
+from calculations.adapters.types.basic_types import ExpenseUUID
 
+
+metadata_obj = MetaData()
+
+expenses = CustomTable(
+    'expense',
+    metadata_obj,
+    CustomColumn.UUID_as_primary_key("id", ExpenseUUID),
+    CustomColumn('description', String(255)),
+    CustomColumn('value', Float(asdecimal=True)),
+    CustomColumn('due_date', DateTime, default=datetime.utcnow),
+    CustomColumn('already_paid', Boolean, default=False),
+    CustomColumn('category', String(100), default='other'),
+)
