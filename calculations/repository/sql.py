a47836af55d4dfba8b7069cd6fdc0d378b73c2a6
Joao Victor <jvpereirarocha@gmail.com>
2022-12-30 01:28:28 -0300

    update repository list of queries

--- calculations/repository/sql.py
+++ calculations/repository/sql.py
@@ -1,9 +1,15 @@
+from typing import Optional
 import uuid
+from calculations.adapters.types.basic_types import UserUUID
 from calculations.repository.base import BaseRepo
 from calculations.domain.entities.user import User
 
 
 class SqlAlchemyRepo(BaseRepo):
-    def consultar_primeiro_usuario(self) -> User:
+    def consultar_primeiro_usuario(self) -> Optional[User]:
         with self:
             return self.session.query(User).first()
+
+    def consultar_usuario_por_id(self, usuario_id: UserUUID) -> Optional[User]:
+        with self:
+            return self.session.query(User.user_id == usuario_id).first()
