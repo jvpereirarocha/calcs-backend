ec2e733126baf25b0ed6d582c5fab79904b8a517
Joao Victor Rocha <jvpereirarocha@gmail.com>
2023-01-02 19:53:28 -0300

    using the current config_name environment variable to set the correctly config

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+from infrastructure.config.configuration_objects import environments
 from calculations.adapters.orms.mappers import start_mappers
 
 def create_app(config_name: str):
     app = Flask(__name__)
+    current_configuration = environments.get(config_name)
+    app.config.from_object(current_configuration)
     start_mappers()
     return app
