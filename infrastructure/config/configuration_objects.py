5014eaeda5a924c6cd21f2142c2972e758387602
Joao Victor Rocha <jvpereirarocha@gmail.com>
2023-01-02 19:52:47 -0300

    create classes to manage the current configuration

---
+++ infrastructure/config/configuration_objects.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from os import getenv
+
+
+class BaseConfig:
+    IS_TESTING = False
+    FLASK_ENV = getenv("FLASK_ENV")
+    DATABASE_URI = getenv("DATABASE_URI")
+
+
+class DevelopmentConfig(BaseConfig):
+    IS_TESTING = False
+
+
+class ProductionConfig(BaseConfig):
+    IS_TESTING = False
+
+
+class TestingConfig(BaseConfig):
+    IS_TESTING = True
+    DATABASE_URI = "sqlite:///app-test.db"
+
+
+
+environments = {
+    "production": ProductionConfig(),
+    "development": DevelopmentConfig(),
+    "testing": TestingConfig(),
+}
