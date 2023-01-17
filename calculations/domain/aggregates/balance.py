b17d6c340ae044266098b1e592f703ac5f32eb1c
Joao Victor <jvpereirarocha@gmail.com>
2023-01-17 00:02:32 -0300

    update aggregate

--- calculations/domain/aggregates/balance.py
+++ calculations/domain/aggregates/balance.py
@@ -1,32 +1,36 @@
 from dataclasses import dataclass, field
-from datetime import datetime
+from enum import Enum
+from datetime import date, datetime
 from typing import List, Optional
-from calculations.adapters.types.basic_types import BalanceUUID, PersonUUID
+from calculations.adapters.types.basic_types import BalanceUUID
 from calculations.adapters.types.date_hour import DateRange
-from calculations.domain.entities.accounts import Account
 from calculations.domain.entities.expenses import Expense
-from calculations.domain.entities.person import Person
 from calculations.domain.entities.revenues import Revenue
+
+
+class StatusBalance(Enum):
+    INITIAL = "initial"
+    ON_PROCESS = "on process"
+    FINISHED = "finished"
 
 
 @dataclass
 class Balance:
     balance_id: BalanceUUID
+    description: str
     month: int
     year: int
-    start_date: datetime
-    end_date: datetime
-    value: float
-    accounts: Optional[List[Account]] = field(default_factory=list)
+    start_date: date
+    end_date: date
+    total_of_balance: float
     expenses: Optional[List[Expense]] = field(default_factory=list)
     revenues: Optional[List[Revenue]] = field(default_factory=list)
+    status_balance: str = StatusBalance.INITIAL.value
+    created_when: Optional[datetime] = None
+    modified_when: Optional[datetime] = None
 
     def __str__(self):
         return f"<Balance of {self.month}/{self.year} amount - R$: {self.value}"
-
-    @property
-    def sum_account_values(self):
-        return sum([account.amount for account in self.accounts])
 
     @property
     def sum_of_all_expenses_on_month(self) -> float:
@@ -41,7 +45,7 @@ class Balance:
         return DateRange(start=self.start_date, end=self.end_date)
 
     def sum_balance_based_on_month_transactions(self) -> float:
-        return (self.sum_account_values + self.sum_of_all_revenues) - self.sum_of_all_expenses_on_month
+        return (self.total_of_balance + self.sum_of_all_revenues) - self.sum_of_all_expenses_on_month
 
     def balance_with_negative_value(self) -> bool:
         return self.sum_balance_based_on_month_transactions() < 0
