1d64bfe6c9ae62d73bf009c7777d746e88c6a3a6
Joao Victor <jvpereirarocha@gmail.com>
2023-05-01 22:00:07 -0300

    creating new user repo queries

--- infrastructure/database/repository/users/user_repo.py
+++ infrastructure/database/repository/users/user_repo.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Iterable, Optional
 from libs.types.identifiers import UserUUID
 from calculations.domain.abstractions.repository.user.abstract_repo_user import AbstractUserRepo
 from calculations.domain.entities.user import User
@@ -13,3 +13,12 @@ class UserRepo(SqlBaseRepo, AbstractUserRepo):
     def get_first_user_by_id(self, user_id: UserUUID) -> Optional[User]:
         with self:
             return self.session.query(User.user_id == user_id).first()
+        
+    def save_user(self, user: User) -> None:
+        with self:
+            self.session.add(user)
+            self.session.commit()
+
+    def get_all_users(self) -> Iterable[User]:
+        with self:
+            return self.session.query(User).all()
