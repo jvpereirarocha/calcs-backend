b327deef7273a68096941b1d69141bcac8e1636a
Joao Victor <jvpereirarocha@gmail.com>
2023-05-01 21:59:06 -0300

    creating ports to parse requesting data

---
+++ application/ports/user.py
@@ -0,0 +1,12 @@
+from calculations.domain.abstractions.port.abstract_port import AbstractInputPort
+from dataclasses import dataclass
+
+from libs.types.identifiers import UserUUID
+
+
+@dataclass
+class CreateUserInputPort(AbstractInputPort):
+    user_id: UserUUID
+    email: str
+    password: str
+    avatar: str
