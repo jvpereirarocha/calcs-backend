052a2bf6739774dd6ad32eca00df064364122291
Joao Victor <jvpereirarocha@gmail.com>
2023-05-01 21:59:25 -0300

    add business logic to user entity

--- calculations/domain/entities/user.py
+++ calculations/domain/entities/user.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Optional, List
+from typing import Optional, List, Self
 
 from libs.types.identifiers import UserUUID
 from calculations.domain.entities.models import BaseModel
+import bcrypt
 
 
 @dataclass
@@ -12,3 +13,17 @@ class User(BaseModel):
     email: str
     password: str
     avatar: Optional[str] = None
+
+    @classmethod
+    def _encrypt_password(self, password: str) -> str:
+        salt = bcrypt.gensalt(rounds=32)
+        return bcrypt.hashpw(password=password, salt=salt)
+
+    @classmethod
+    def create_user(cls, user_id: UserUUID, email: str, password: str, avatar: Optional[str] = None) -> Self:
+        return cls(
+            user_id=user_id,
+            email=email,
+            password=cls._encrypt_password(password=password),
+            avatar=avatar
+        )
