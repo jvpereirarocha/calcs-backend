916b58e16fbfb77d5d7e53753c0f7e9d2f53eca3
Joao Victor <jvpereirarocha@gmail.com>
2023-05-02 21:32:33 -0300

    registering blueprint

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from infrastructure.config.configuration_objects import environments
-from calculations.adapters.orms.mappers import start_mappers
+from infrastructure.database.orms.mappers import start_mappers
+from application.endpoints import api_v1_blueprint
 
 def create_app(config_name: str):
     app = Flask(__name__)
     current_configuration = environments.get(config_name)
     app.config.from_object(current_configuration)
+    app.register_blueprint(api_v1_blueprint)
     start_mappers()
     return app
