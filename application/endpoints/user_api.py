fe4c75b44deed1dcad620ea40eb7fbd6d0402be3
Joao Victor <jvpereirarocha@gmail.com>
2023-05-16 00:08:52 -0300

    changing endpoints get_all_users and create_user

--- application/endpoints/user_api.py
+++ application/endpoints/user_api.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
+from application.parsers.user import AllUsersParser, CreatedOrUpdatedUserParser
 
 from application.ports.user import CreateUserInputPort
-from application.services.user_service import CreateUserService
+from application.services.user_service import CreateUserService, GetUsersService
 from infrastructure.database.repository.users.user_repo import UserRepo
 from libs.types.identifiers import UserUUID
 
@@ -11,22 +12,22 @@ user_blueprint = Blueprint("users", __name__, url_prefix="/users")
 
 @user_blueprint.route("/", methods=["GET"])
 def get_all_users():
-    pass
+    service = GetUsersService(repo=UserRepo())
+    users = service.get_all()
+    parser = AllUsersParser()
+    return parser.to_json(data=users), 200
 
 @user_blueprint.route("/new", methods=["POST"])
 def create_user():
-    
     data = request.get_json()
-
     user_requester = CreateUserInputPort(
         user_id=UserUUID(),
         email=data["email"],
         password=data["password"],
         avatar=data.get("avatar", None)
     )
-
     repo = UserRepo()
     service = CreateUserService(requester=user_requester, repo=repo)
-    service.operation()
-
-    return jsonify({"User is created!"}), 201
+    service.create_or_update()
+    parser = CreatedOrUpdatedUserParser()
+    return parser.to_json(), 201
