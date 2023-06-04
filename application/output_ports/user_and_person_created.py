88d037dfd62cc08d513552dad196c4b2b2ab1d47
Joao Victor Rocha <jvpereirarocha@gmail.com>
2023-06-04 19:27:39 -0300

    creating output port returning json response

---
+++ application/output_ports/user_and_person_created.py
@@ -0,0 +1,29 @@
+import json
+from calculations.domain.abstractions.port.abstract_port import AbstractOutputPort
+from calculations.domain.entities.person import Person
+from calculations.domain.entities.user import User
+
+
+class UserAndPersonCreated(AbstractOutputPort):
+    def __init__(self, person: Person, user: User):
+        self.person = person
+        self.user = user
+
+    def to_json(self):
+        try:
+            data = {
+                "success": {
+                    "person": self.person.to_dict(),
+                    "user": self.user.to_dict()
+                }
+            }
+            serialized_data = json.dumps(data)
+            self.status_code = 201
+            return serialized_data
+        except Exception as error:
+            self.status_code = 500
+            return json.dumps({
+                "error": {
+                    "message": str(error)
+                }
+            })
