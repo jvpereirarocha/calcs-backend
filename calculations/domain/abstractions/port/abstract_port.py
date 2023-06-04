528020f7bc8fe047b2df2d934789a455460983ca
Joao Victor Rocha <jvpereirarocha@gmail.com>
2023-06-04 19:28:23 -0300

    changing abstract ports (inputs and outputs)

--- calculations/domain/abstractions/port/abstract_port.py
+++ calculations/domain/abstractions/port/abstract_port.py
@@ -1,6 +1,24 @@
 from datetime import datetime
+from abc import ABC, abstractmethod
 
 
-class AbstractInputPort:
+class AbstractInputPort(ABC):
     def __init__(self, created_at: datetime = datetime.now()) -> None:
         self.created_at = created_at
+
+    @abstractmethod
+    def validate_request(self):
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def request_is_valid(self):
+        raise NotImplementedError()
+    
+
+class AbstractOutputPort(ABC):
+    def __init__(self) -> None:
+        self.status_code = 200
+    
+    @abstractmethod
+    def to_json(self, data):
+        raise NotImplementedError()
