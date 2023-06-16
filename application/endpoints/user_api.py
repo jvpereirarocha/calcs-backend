8ccb39e4260bfc610f47775688e39a1015a4e59c
Joao Victor <jvpereirarocha@gmail.com>
2023-06-16 00:37:12 -0300

    fixing user create resource from api

--- application/endpoints/user_api.py
+++ application/endpoints/user_api.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
 from application.input_ports.person import CreatePerson
 
 from application.input_ports.user import CreateUser
 from application.output_ports.user_and_person_created import UserAndPersonCreated
 from application.services.person_service import CreatePersonService
-from application.services.user_service import CreateUserService, FetchOneUserService, GetUsersService
+from application.services.user_service import CreateUserService
 from infrastructure.database.repository.persons.person_repo import PersonRepo
 from infrastructure.database.repository.users.user_repo import UserRepo
 from libs.types.identifiers import PersonUUID, UserUUID
@@ -16,15 +16,15 @@ user_blueprint = Blueprint("users", __name__, url_prefix="/users")
 def create_user_and_person():
     data = request.get_json()
     # First of all, creating a user
-    user_requester = CreateUser(
+    user_data = CreateUser(
         user_id=UserUUID(),
         email=data["email"],
         password=data["password"],
         avatar=data.get("avatar", None)
     )
-    user_requester.validate_request()
+    
     user_repo = UserRepo()
-    user_service = CreateUserService(requester=user_requester, repo=user_repo)
+    user_service = CreateUserService(requester=user_data, repo=user_repo)
     user = user_service.create_or_update()
     user_repo.commit()
     # After that, creating a person instance
@@ -34,14 +34,14 @@ def create_user_and_person():
         last_name=data["last_name"],
         date_of_birth=data["date_of_birth"]
     )
-    person_requester.validate_request()
+
     person_repo = PersonRepo()
     person_service = CreatePersonService(requester=person_requester, repo=person_repo)
     person = person_service.create_or_update()
     person_repo.commit()
 
-    output = UserAndPersonCreated(
+    response = UserAndPersonCreated(
         user=user,
         person=person
     )
-    return output.to_json(), output.status_code
+    return jsonify(response.to_json()), response.status_code
