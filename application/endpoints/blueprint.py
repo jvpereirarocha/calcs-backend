f4c021a71a9bfb930313584e6d2185d62e6bf4dd
Joao Victor <jvpereirarocha@gmail.com>
2023-06-16 00:33:51 -0300

    configure after_request to avoid cors errors

--- application/endpoints/blueprint.py
+++ application/endpoints/blueprint.py
@@ -1,4 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
 
 
 api_v1_blueprint = Blueprint("api_v1", __name__, url_prefix="/api/v1")
+
+
+@api_v1_blueprint.after_request
+def set_response_after_request():
+    response = make_response()
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Content-Type"] = "application/json"
+    return response
