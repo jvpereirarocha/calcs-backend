95a16f3f64d30734e3ff0e3bd8d2bc88d8ff77a7
Joao Victor <jvpereirarocha@gmail.com>
2023-06-16 00:35:37 -0300

    fixing all queries, changing to new pattern of sqlalchemy

--- infrastructure/database/repository/users/user_repo.py
+++ infrastructure/database/repository/users/user_repo.py
@@ -1,4 +1,6 @@
 from typing import Iterable, Optional
+
+from sqlalchemy import select
 from libs.types.identifiers import UserUUID
 from calculations.domain.abstractions.repository.user.abstract_repo_user import AbstractUserRepo
 from calculations.domain.entities.user import User
@@ -8,11 +10,18 @@ from infrastructure.database.repository.base import SqlBaseRepo
 class UserRepo(SqlBaseRepo, AbstractUserRepo):
     def get_first_user(self) -> Optional[User]:
         with self:
-            return self.session.query(User).first()
+            query = select(User).order_by(User.email)
+            return self.session.execute(query).scalar_one_or_none()
 
     def get_first_user_by_id(self, user_id: UserUUID) -> Optional[User]:
         with self:
-            return self.session.query(User.user_id == user_id).first()
+            query = select(User).where(User.user_id == user_id)
+            return self.session.execute(query).scalar_one_or_none()
+        
+    def get_first_by_email(self, email: str) -> Optional[User]:
+        with self:
+            query = select(User).where(User.email == email)
+            return self.session.execute(query).scalar_one_or_none()
         
     def save_user(self, user: User) -> None:
         with self:
@@ -21,7 +30,8 @@ class UserRepo(SqlBaseRepo, AbstractUserRepo):
 
     def get_all_users(self) -> Iterable[User]:
         with self:
-            return self.session.query(User).all()
+            query = select(User).order_by(User.email)
+            return self.session.execute(query).scalars()
         
     def commit(self) -> None:
         with self:
