4fa9445e1dff0b3852fb38bcbff65ade2726dd61
Joao Victor <jvpereirarocha@gmail.com>
2023-06-16 23:39:26 -0300

    remove unused files

--- tests/domain/test_consistency.py
+++
@@ -1,9 +0,0 @@
-from calculations.domain.aggregates.balance import Balance
-
-
-def test_balance_with_negative_value(
-    mock_balance,
-):
-    balance: Balance = mock_balance
-
-    assert balance.balance_with_negative_value() is False
