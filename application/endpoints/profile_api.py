36c1d79567c79bd584d844d230363d64e6199a08
Joao Victor <jvpr.tech@gmail.com>
2023-06-18 21:27:14 -0300

    change register new profile endpoint

--- application/endpoints/profile_api.py
+++ application/endpoints/profile_api.py
@@ -15,31 +15,36 @@ profile_blueprint = Blueprint("profile", __name__, url_prefix="/profile")
 def register_new_profile():
     data = request.get_json()
     # First of all, creating a user
-    user_data = CreateUser(
+    user_requester = CreateUser(
         user_id=UserUUID(),
         email=data["email"],
         password=data["password"],
         avatar=data.get("avatar", None)
     )
+
+    user_requester.validate_request()
     
     profile_repo = ProfileRepo()
-    user_service = CreateUserService(requester=user_data, repo=profile_repo)
+    user_service = CreateUserService(requester=user_requester, repo=profile_repo)
     user = user_service.create_or_update()
     # After that, creating a person instance
     person_requester = CreatePerson(
         person_id=PersonUUID(),
-        first_name=data["first_name"],
-        last_name=data["last_name"],
-        date_of_birth=data["date_of_birth"]
+        first_name=data["firstName"],
+        last_name=data["lastName"],
+        date_of_birth=data["dateOfBirth"],
+        user_id=user.user_id
     )
+    person_requester.validate_request()
 
     person_service = CreatePersonService(requester=person_requester, repo=profile_repo)
     person = person_service.create_or_update()
     
     profile_repo.commit()
 
-    response = UserAndPersonCreated(
+    output = UserAndPersonCreated(
         user=user,
         person=person
     )
-    return jsonify(response.to_json()), response.status_code
+    response, status_code = output.to_json()
+    return response, status_code
