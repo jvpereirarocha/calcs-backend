0a984233d4fda5455dc713d78cc353d2b32c2c8b
Joao Victor <jvpr.tech@gmail.com>
2023-06-20 00:32:29 -0300

    unifying wxpense and revenue repos into balance repo

---
+++ calculations/domain/abstractions/repository/balances/abstract_repo_balance.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from calculations.domain.abstractions.repository.base.abstract_repo import AbstractRepo
+from calculations.domain.aggregates.balance import Balance
+from calculations.domain.entities.expenses import Expense
+from calculations.domain.entities.revenues import Revenue
+
+
+class AbstractBalanceRepo(AbstractRepo, ABC):
+    @abstractmethod
+    def save_balance(self, balance: Balance) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def save_revenue(self, revenue: Revenue) -> None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def save_expense(self, expense: Expense) -> None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def commit(self) -> None:
+        raise NotImplementedError()
