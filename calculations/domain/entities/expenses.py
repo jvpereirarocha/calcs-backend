2a84512a53f5448dd020584c1095f08ccb86d4a2
Joao Victor <jvpr.tech@gmail.com>
2023-06-20 00:38:02 -0300

    adding classmethod to create new_expense

--- calculations/domain/entities/expenses.py
+++ calculations/domain/entities/expenses.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Self
+from calculations.domain.value_object.expense_category import ExpenseCategory
 from libs.types.identifiers import ExpenseUUID, PersonUUID, BalanceUUID
 
 from calculations.domain.entities.models import InheritedModel
@@ -16,3 +17,26 @@ class Expense(InheritedModel):
     person_id: Optional[PersonUUID] = None
     category: Optional[str] = None
     balance_id: Optional[BalanceUUID] = None
+
+    @classmethod
+    def create_new_expense(
+        cls,
+        description: str,
+        value: float,
+        due_date: datetime,
+        person_id: PersonUUID,
+        category: str,
+        balance_id: BalanceUUID,
+        already_paid: bool = False,
+    ) -> Self:
+
+        return cls(
+            expense_id=ExpenseUUID(),
+            description=description,
+            value=value,
+            due_date=due_date,
+            already_paid=already_paid,
+            person_id=person_id,
+            category=category,
+            balance_id=balance_id
+        )
