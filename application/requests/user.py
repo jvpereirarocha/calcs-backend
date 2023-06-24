382167199cee4bdff2c5b53be02f5c79126ef8fd
Joao Victor <jvpr.tech@gmail.com>
2023-06-23 22:04:47 -0300

    validating login request

--- application/requests/user.py
+++ application/requests/user.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from application.requests.mixins import UserValidatorMixin
 from calculations.domain.abstractions.port.abstract_port import AbstractInputPort
 from dataclasses import dataclass
 
@@ -6,7 +7,7 @@ from libs.types.identifiers import UserUUID
 
 
 @dataclass
-class CreateUser(AbstractInputPort):
+class CreateUser(AbstractInputPort, UserValidatorMixin):
     user_id: UserUUID
     email: str
     password: str
@@ -14,18 +15,26 @@ class CreateUser(AbstractInputPort):
 
     _valid_request: bool = False
 
-    def _validate_email(self):
-        if not self.email:
-            raise ValueError("Email is required.")
-
-    def _validate_password(self):
-        if not self.password:
-            raise ValueError("Password is required.")
-
     def validate_request(self):
-        self._validate_email()
-        self._validate_password()
+        self.validate_email()
+        self.validate_password()
         self._valid_request = True
 
     def request_is_valid(self):
         return self._valid_request
+    
+
+@dataclass
+class LoginRequest(AbstractInputPort, UserValidatorMixin):
+    email: str
+    password: str
+
+    _validate_request: bool = False
+
+    def validate_login(self):
+        self.validate_email()
+        self.validate_password()
+        self._validate_request = True
+    
+    def request_is_valid(self):
+        return self._validate_request
