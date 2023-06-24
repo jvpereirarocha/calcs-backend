8149fb66fef83c015a24bc35babffd28d1a1ed3c
Joao Victor <jvpr.tech@gmail.com>
2023-06-23 23:37:19 -0300

    handling errors in response

--- application/responses/login_response.py
+++ application/responses/login_response.py
@@ -1,19 +1,28 @@
 import json
+from typing import List
 from calculations.domain.abstractions.port.abstract_port import AbstractOutputPort
 
 
 class LoginResponse(AbstractOutputPort):
-    def __init__(self, email: str, token: str):
+    def __init__(self, email: str, token: str, errors: List[str] = []):
         self.email = email
         self.token = token
-        self.status_code = 200
+        self.errors = errors
+        self.status_code = None
 
     def to_json(self):
         try:
-            data = {
-                "email": self.email,
-                "token": self.token,
-            }
+            if not self.errors:
+                data = {
+                    "email": self.email,
+                    "token": self.token,
+                }
+                self.status_code = 200
+            else:
+                data = {
+                    "errors": [error for error in self.errors] 
+                }
+                self.status_code = 400
             serialized_data = json.dumps(data)
             return serialized_data, self.status_code
         except Exception as error:
