f0141a676b5f6ee312b6983449db41557094f767
Joao Victor <jvpr.tech@gmail.com>
2023-09-07 12:48:33 -0300

    fixing user service

--- application/services/user_service.py
+++ application/services/user_service.py
@@ -17,20 +17,21 @@ class CreateUserService(AbstractCreateOrUpdateService):
     def __init__(self, requester: CreateUser, repo: AbstractProfileRepo):
         self.requester = requester
         self.repo = repo
+        self.error = ""
+        self.user = None
 
-    def _create_new_user(self):
-        user_already_exists = self.repo.get_first_user_by_id(
-            user_id=self.requester.user_id
+    def _validate_new_user(self):
+        user_can_be_created = True
+        user_already_exists = self.repo.get_first_user_by_email(
+            email=self.requester.email
         )
         if user_already_exists:
-            user_already_exists.update_user(
-                email=self.requester.email,
-                password=self.requester.password,
-                avatar=self.requester.avatar,
-            )
-            self.repo.save_user(user=user_already_exists)
-            return user_already_exists
+            self.error = "This user is already registered"
+            user_can_be_created = False
 
+        return user_can_be_created
+
+    def _create_new_user(self):
         user = User.create_user(
             user_id=self.requester.user_id,
             email=self.requester.email,
@@ -38,11 +39,16 @@ class CreateUserService(AbstractCreateOrUpdateService):
             avatar=self.requester.avatar,
         )
         self.repo.save_user(user=user)
-        return user
+        self.user = user
+        return self
 
     def create_or_update(self):
+        user_is_valid_an_can_be_created = self._validate_new_user()
+        if not user_is_valid_an_can_be_created:
+            return self
+
         return self._create_new_user()
-    
+
 
 class LoginService(AbstractFetchOneService):
     def __init__(self, requester: LoginRequest, repo: AbstractProfileRepo, error: str):
@@ -51,27 +57,23 @@ class LoginService(AbstractFetchOneService):
         self.error = error
 
     def fetch_one(self, entity_id: UserUUID) -> Optional[User]:
-        return self.repo.get_first_user_by_id(
-            user_id=entity_id
-        )
+        return self.repo.get_first_user_by_id(user_id=entity_id)
 
     def fetch_by_email(self) -> Optional[User]:
-        return self.repo.get_first_user_by_email(
-            email=self.requester.email
-        )
-    
+        return self.repo.get_first_user_by_email(email=self.requester.email)
+
     def make_login(self) -> Optional[str]:
         user: Optional[User] = self.fetch_by_email()
-        encrypted_password = User.encrypt_password(
-            password=self.requester.password
-        )
-        if user and user.email == self.requester.email and user.password == encrypted_password:
+        if (
+            user
+            and user.email == self.requester.email
+            and user.check_password(password_to_verify=self.requester.password)
+        ):
             return user.get_token(secret_key=getenv("JWT_SECRET_KEY"))
         else:
             error = "Invalid credentials"
             self.error = error
-        
-            
+
 
 class GetUsersService(AbstractGetAllService):
     def __init__(self, repo: AbstractProfileRepo) -> None:
